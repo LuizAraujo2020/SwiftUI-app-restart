ae7ac3cd924b00bc94df5c9b05dc19c5b635df04
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-11-25 12:33:21 -0300

    Complex SwiftUI Drag Gesture

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive = true
 
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0.0
+
     var body: some View {
         ZStack {
             Color("ColorBlue")
@@ -72,7 +75,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
 
                         Spacer()
                     }
@@ -91,14 +94,29 @@ struct OnboardingView: View {
                         }
                         .foregroundStyle(.white)
                         .frame(width: 80, height: 80)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 &&
+                                    buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                }
+                        )
 
                         Spacer()
                     } //: hstack
                 } //: footer
-                .frame(height: 80)
+                .frame(width: buttonWidth, height: 80)
                 .padding()
 
             } //: vstack
